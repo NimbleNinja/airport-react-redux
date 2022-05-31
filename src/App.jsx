e5dd04ddf146cf42c62c4b641dce3cb1c8815d5f
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-05-31 11:16:44 +0300

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Flights from './features/flights/components/Flights';
 import Search from './features/search/Search';
 import store from './store';
 
 const App = () => (
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <div className="page">
         <h1 className="page__title">Search flight</h1>
         <Search />
         <Flights />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
